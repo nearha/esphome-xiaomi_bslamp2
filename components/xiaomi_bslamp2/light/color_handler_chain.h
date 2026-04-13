180f6fd639d7c856bdc2138422f4f9c4b4f22e23
nearha <114181979+nearha@users.noreply.github.com>
2026-04-12 21:13:32 -0300

    Update color_handler_chain.h

--- components/xiaomi_bslamp2/light/color_handler_chain.h
+++ components/xiaomi_bslamp2/light/color_handler_chain.h
@@ -8,7 +8,6 @@
 #include "color_handler_off.h"
 #include "color_handler_rgb.h"
 #include "color_handler_color_temperature.h"
-#include "color_handler_night_light.h"
 
 namespace esphome {
 namespace xiaomi {
@@ -18,30 +17,24 @@ namespace bslamp2 {
  * This class translates LightColorValues into GPIO duty cycles that can be
  * used for representing a requested light color on the physical device.
  *
- * The code handles all known light modes for the device:
+ * In this fork, night light mode is disabled.
  *
- * - off: the light is off
- * - night light: based on RGB or white mode + lowest possible brightness
+ * The code handles:
+ * - off
  * - white light: based on color temperature + brightness
  * - RGB light: based on RGB values + brightness
  */
 class ColorHandlerChain : public ColorHandler {
  public:
   bool set_light_color_values(light::LightColorValues v) {
-    // The actual implementation of the various light modes is in separate
-    // targeted classes. These classes are called here in a chain of
-    // command-like pattern, to let the first one that can handle the light
-    // settings do the honours.
     if (off_light_.set_light_color_values(v))
       off_light_.copy_to(this);
-    else if (night_light_.set_light_color_values(v))
-      night_light_.copy_to(this);
     else if (white_light_.set_light_color_values(v))
       white_light_.copy_to(this);
     else if (rgb_light_.set_light_color_values(v))
       rgb_light_.copy_to(this);
     else {
-      ESP_LOGE(TAG, "Light color error: (None of the ColorHandler classes handles the requested light state; defaulting to 'off'");
+      ESP_LOGE(TAG, "Light color error: (None of the ColorHandler classes handles the requested light state; defaulting to 'off')");
       off_light_.copy_to(this);
     }
 
@@ -49,10 +42,9 @@ class ColorHandlerChain : public ColorHandler {
   }
 
  protected:
-  ColorHandlerOff              off_light_;
-  ColorHandlerRGB              rgb_light_;
+  ColorHandlerOff off_light_;
+  ColorHandlerRGB rgb_light_;
   ColorHandlerColorTemperature white_light_;
-  ColorHandlerNightLight       night_light_;
 };
 
 }  // namespace bslamp2
